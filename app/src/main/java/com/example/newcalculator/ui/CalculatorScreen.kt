65474368cb5108b390d080b7fb658b451a8cc94b
Babak <babakhanibabak@gmail.com>
2025-06-05 15:09:18 +0330

    Create CalculatorScreen

--- app/src/main/java/com/example/newcalculator/ui/CalculatorScreen.kt
+++ app/src/main/java/com/example/newcalculator/ui/CalculatorScreen.kt
@@ -1,21 +1,37 @@
 package com.example.newcalculator.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.newcalculator.ui.theme.NewCalculatorTheme
 
 @Composable
 fun CalculatorScreen(
-    modifier: Modifier = Modifier
+    viewModel: CalculatorScreenViewModel = hiltViewModel(),
 ) {
-    CalculatorScreenContent()
+    val uiState by viewModel.uiState.collectAsState()
+
+    CalculatorScreenContent(
+        uiState = uiState
+    )
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalculatorScreenContent(
-    modifier: Modifier = Modifier
+    uiState: CalculatorScreenUiState,
+    onOperatorClick:(CalculatorOperator)->Unit={}
 ) {
 
 }
@@ -24,6 +40,8 @@ fun CalculatorScreenContent(
 @Composable
 private fun CalculatorScreenContentPreview() {
     NewCalculatorTheme {
-        CalculatorScreenContent()
+        CalculatorScreenContent(
+            uiState = CalculatorScreenUiState()
+        )
     }
 }
