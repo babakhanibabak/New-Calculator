314e0f13e526e07a08869c7c8740c8988b87ea28
Babak <babakhanibabak@gmail.com>
2025-06-06 13:11:58 +0330

    Complete CalculatorScreen

--- app/src/main/java/com/example/newcalculator/ui/CalculatorScreen.kt
+++ app/src/main/java/com/example/newcalculator/ui/CalculatorScreen.kt
@@ -3,16 +3,14 @@ package com.example.newcalculator.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.newcalculator.ui.component.CalculatorAppBar
 import com.example.newcalculator.ui.theme.NewCalculatorTheme
 
 @Composable
@@ -27,7 +25,6 @@ fun CalculatorScreen(
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalculatorScreenContent(
     uiState: CalculatorScreenUiState,
@@ -35,11 +32,7 @@ fun CalculatorScreenContent(
 ) {
     Scaffold(modifier = Modifier.fillMaxSize(),
         topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "")
-                }
-            )
+           CalculatorAppBar(title = "Calculator")
         }
     ) {paddingValues ->
         Column (
