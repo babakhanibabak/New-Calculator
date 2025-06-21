a545f1f2607b0f24635d9689e9192d2657a9e0c3
Babak <babakhanibabak@gmail.com>
2025-06-22 00:28:09 +0330

    Install math view library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     // Hilt Navigation Compose
     implementation(libs.hilt.navigation.compose)
 
+    //math
+    implementation(libs.math.view)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
